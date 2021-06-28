43d3021ab6d53453079e21c33b90084f734209ef
tongshuyang <1378022352@qq.com>
2021-06-29 00:12:56 +0800

    修改实体类公司类

--- src/main/java/cn/litman/fist/entity/Case.java
+++ src/main/java/cn/litman/fist/entity/Case.java
@@ -5,35 +5,78 @@ import lombok.Setter;
 
 import java.util.Date;
 
+
 @Getter
 @Setter
 public class Case {
-    private Integer caseId;
+    /**
+     *案例id
+     */
+    private Integer id;
 
+    /**
+    *用户id
+    */
     private Integer uId;
 
+    /**
+     *客户id
+     */
     private Integer cId;
 
+    /**
+     *禅道id
+     */
     private Integer storyId;
 
+    /**
+     *标题
+     */
     private String title;
 
+    /**
+     *案例描述
+     */
     private String description;
 
+    /**
+     *关键错误代码
+     */
     private String errorCode;
 
+    /**
+     *问题分类：0 :未分类  1：API 2：Android 3：iOS 4：JSSDK 5：小程序 6：使用问题
+     */
     private Byte category;
 
+    /**
+     *状态：0 :活动  1：关闭
+     */
     private Byte status;
 
+    /**
+     *产品类型：0 :未分类  1：大班课-直播 2：大班课-回放： 3：小班课-直播 4：小班课-回放 5：点播
+     */
     private Byte productType;
 
+    /**
+     *跟进信息记录
+     */
     private String followInfo;
 
+    /**
+     *解决方式总结
+     */
     private String solution;
 
+    /**
+     *创建时间
+     */
     private Date createTime;
 
+    /**
+     *更新时间
+     */
     private Date updateTime;
 
 }
--- src/main/java/cn/litman/fist/entity/Customer.java
+++ src/main/java/cn/litman/fist/entity/Customer.java
@@ -5,28 +5,67 @@ import lombok.Setter;
 
 import java.util.Date;
 
+/**
+ * 客户信息实体类
+ * @author SoyungTong
+ * @email litman@126.com
+ * @date 2021/6/15 21:14
+ */
 @Getter
 @Setter
 public class Customer {
-    private Integer cId;
+    /**
+    *顾客id
+    */
+    private Integer id;
 
+    /**
+    *公司名称
+    */
     private String companyName;
 
+    /**
+    *所属销售
+    */
     private String saleName;
 
+    /**
+     *所属区域
+     */
     private Byte area;
 
+    /**
+     *规模等级
+     */
     private Byte level;
 
+    /**
+     *是否签约
+     */
     private Byte isSigned;
 
+    /**
+     *公司详情
+     */
     private String companyDetail;
 
+    /**
+     *产品对接方式
+     */
     private String buttType;
 
+    /**
+     *对接进度
+     */
     private Byte buttProgress;
 
+    /**
+     *添加时间
+     */
     private Date createTime;
 
+    /**
+     *更新时间
+     */
     private Date updateTime;
 }
